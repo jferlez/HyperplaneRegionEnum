Fix normalization bugs

region_helpers.py
@@ -106,8 +106,8 @@ class flipConstraints:
 
         self.allNrms = np.vstack([ self.allNrms[:self.allN,], np.array([[1]],dtype=np.int64), self.allNrms[self.allN:,] ])
         if not self.normalize is None:
-            self.allNrms[self.allN,] = self.normalize / np.linalg.norm(self.constraints[self.allN,1:].reshape(1,-1),axis=1).reshape(-1,1)
-            self.allConstraints[self.allN,:] = self.allNrms[self.allN] * self.allConstraints[self.allN,:]
+            self.allNrms[self.allN,] = self.normalize / np.linalg.norm(self.allConstraints[self.allN,1:].reshape(1,-1),axis=1).reshape(-1,1)
+            self.allConstraints = self.allNrms * self.allConstraints
         else:
             self.normalize = None
 
@@ -296,7 +296,7 @@ class flipConstraintsReducedMin(flipConstraints):
         self.nonRedundantHyperplanes = np.nonzero(self.redundantFlips > 0)[0]
 
         self.nrms = np.vstack([ self.allNrms[self.nonRedundantHyperplanes,].reshape(-1,1), self.allNrms[self.allN:,].reshape(-1,1) ])
-        self.constraints = self.nrms * np.vstack( \
+        self.constraints = np.vstack( \
                                     ( \
                                         self.allConstraints[self.nonRedundantHyperplanes,:], \
                                         self.allConstraints[self.allN:,:] \
@@ -326,7 +326,7 @@ class flipConstraintsReducedMin(flipConstraints):
             self.nonRedundantHyperplanes = np.nonzero(self.redundantFlips > 0)[0]
 
             self.nrms = np.vstack([ self.allNrms[self.nonRedundantHyperplanes,].reshape(-1,1), self.allNrms[self.allN:,].reshape(-1,1) ])
-            self.constraints = self.nrms * np.vstack( \
+            self.constraints = np.vstack( \
                                         ( \
                                             self.allConstraints[self.nonRedundantHyperplanes,:], \
                                             self.allConstraints[self.allN:,:] \