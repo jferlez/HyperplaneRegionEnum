f5c7e71cf9b3b6fdf2f2b76a9b4df10fd36bf90a
James Ferlez <jferlez@gmail.com>
2024-03-23 20:18:36 -0400

    Fix tabulation of lp counts from different dimensions

--- posetFastCharm.py
+++ posetFastCharm.py
@@ -1009,7 +1009,7 @@ class successorWorker(Chare):
     @coro
     def getStats(self, statsFut):
         if charm.myPe() in self.posetPElist:
-            self.stats['LPSolverCount'] = self.lp.lpCount + self.lpIntPoint.lpCount
+            self.stats['LPSolverCount'] = sum([self.lpObjs[di].lpCount for di in self.lpObjs.keys()])
             self.stats['RSRegionCount'] = self.rsRegionCount
             self.stats['RSLPCount'] = self.rsLP.lpCount + self.rsLPIntPoint.lpCount
         retVal = defaultdict(int) if not charm.myPe() in self.posetPElist else self.stats
